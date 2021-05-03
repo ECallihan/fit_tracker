0c897ef0ebb8bf423b87e5c8542791411f3a0e1a
ECallihan <edward.callihan@gmail.com>
2021-05-03 17:55:59 -0400

    something

--- fit_tracker/settings.py
+++ fit_tracker/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'fit_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fit_tracker',
+#         'USER': 'web_admin',
+#         'PASSWORD': 'pineapple9',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fit_tracker',
-        'USER': 'web_admin',
-        'PASSWORD': 'pineapple9',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
