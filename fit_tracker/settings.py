17de61a27060ffe1e514c9fcb5f833e46e3fe863
ECallihan <edward.callihan@gmail.com>
2021-05-03 17:40:27 -0400

    j

--- fit_tracker/settings.py
+++ fit_tracker/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static')
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR.joinpath('static')
+# ]
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 MEDIA_URL = 'media/'
 
